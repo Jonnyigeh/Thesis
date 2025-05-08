068be92b26cb2d9b6986f1116e73595940ee34f2
Jonny Aarstad Igeh <jonny.igeh@gmail.com>
2025-05-08 18:02:01 +0200

    08.05 - work has been done, some plots were made. Theory section is gtting there.

--- src/simple_time_evolution.py
+++ src/simple_time_evolution.py
@@ -2,63 +2,94 @@ import warnings
 warnings.filterwarnings('ignore', category=UserWarning) # Ignores a warnings due to scipy.special version dislike of current python3.10 installation
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 import scipy
+import seaborn as sns
 from tqdm import tqdm
+from utils.visualization import find_figsize
+def landau_zener(v, Delta):
+    # Define parameters
+    Delta =Delta  # Coupling strength
+    v = v  # Sweeping rate
+    t_values = np.linspace(-5, 5, 1000)  # Time range
+    dt = t_values[1] - t_values[0]  # Time step
+    E0 = 1.0
+    # Define the Hamiltonian function
+    def H(t):
+        epsilon = v * t
+        return np.array([[epsilon, Delta], [Delta, -epsilon]])
+    Hnon = lambda t:  np.array([[v * t , 0], [0, -v * t]])  
+    # Initialize state (start in |1>)
+    psi = np.array([1, 0], dtype=np.complex128)
 
-# Define parameters
-Delta = 1.0  # Coupling strength
-v = 2.0  # Sweeping rate
-t_values = np.linspace(-5, 5, 1000)  # Time range
-dt = t_values[1] - t_values[0]  # Time step
-
-# Define the Hamiltonian function
-def H(t):
-    epsilon = v * t
-    return np.array([[epsilon, Delta], [Delta, -epsilon]])
-Hnon = lambda t:  np.array([[v * t , 0], [0, -v * t]])  
-# Initialize state (start in |1>)
-psi = np.array([1, 0], dtype=complex)
-
-def analytical_eigenvalues(t):
-    epsilon = v * t
-    return np.array([epsilon - np.sqrt(epsilon**2 + Delta**2), epsilon + np.sqrt(epsilon**2 + Delta**2)])
-
-
-# Time evolution
-psi_t = []
-energies = np.zeros((len(t_values), 2))
-nonint_energies = np.zeros_like(energies)
-E0, C0 = np.linalg.eigh(H(t_values[0]))
-psi = C0[:, 0]
-breakpoint()
-for i, t in enumerate(t_values):
-    U = scipy.linalg.expm(-1j * H(t) * dt)  # Time evolution operator
-    psi = U @ psi
-    psi_t.append(psi.copy())
-    energies[i], _ = np.linalg.eigh(H(t))
-    nonint_energies[i], _ = np.linalg.eigh(Hnon(t))
-
-psi_t = np.array(psi_t)
-# Compute probabilities
-P1 = np.abs(psi_t[:, 0])**2
-P2 = np.abs(psi_t[:, 1])**2
-plt.plot(t_values, energies[:,0], 'r-', t_values, energies[:,1], 'b-')
-plt.plot(t_values, nonint_energies[:,0], 'r--', t_values, nonint_energies[:,1], 'b--')
-plt.plot(t_values[::50], analytical_eigenvalues(t_values[::50])[0,:], 'ro', t_values[::50], analytical_eigenvalues(t_values[::50])[1,:], 'bo')
-# plt.scatter(t_values, energies[:,0], t_values, energies[:,1])
-plt.xlabel('Time')
-plt.ylabel('Energy')
-plt.title('Landau-Zener Transition')
-plt.legend(['E1', 'E2'])
-plt.show()
-exit()
+    def analytical_eigenvalues(t):
+        epsilon = v * t
+        return np.array([epsilon - np.sqrt(epsilon**2 + Delta**2), epsilon + np.sqrt(epsilon**2 + Delta**2)])
 
 
+    # Time evolution
+    psi_t = []
+    energies = np.zeros((len(t_values), 2))
+    nonint_energies = np.zeros_like(energies)
+    E0, C0 = np.linalg.eigh(H(t_values[0]))
+    psi = C0[:, 0]
+    for i, t in enumerate(t_values):
+        U = scipy.linalg.expm(-1j * H(t) * dt)  # Time evolution operator
+        psi = U @ psi
+        psi_t.append(psi.copy())
+        energies[i], _ = np.linalg.eigh(H(t))
+        nonint_energies[i], _ = np.linalg.eigh(Hnon(t))
+    
+    return t_values, np.array(psi_t), energies, nonint_energies
+
+matplotlib.style.use('seaborn-v0_8')
+colors = sns.color_palette()
+b = colors[0]
+r = colors[2]
+
+# t_values, psi_t, energies, nonint_energies = landau_zener(2.0, 1.0)
+# psi_t = np.array(psi_t)
+# # Compute probabilities
+# P1 = np.abs(psi_t[:, 0])**2
+# P2 = np.abs(psi_t[:, 1])**2
+# fig, ax = plt.subplots(figsize=find_figsize(1.2, 0.4))
+# ax.plot(t_values, energies[:,0], linestyle='-', color=r, label=r'$E_1$ (coupled)')
+# ax.plot(t_values, nonint_energies[:,0], color=r, linestyle='--', label=r'$E_1$')
+# ax.plot(t_values, energies[:,1], linestyle='-', color=b, label=r'$E_2$ (coupled)')
+# ax.plot(t_values, nonint_energies[:,1], linestyle='--', color=b, label=r'$E_2$')
+# # ax.plot(t_values[::50], analytical_eigenvalues(t_values[::50])[0,:], 'ro', t_values[::50], analytical_eigenvalues(t_values[::50])[1,:], 'bo')
+# # ax.scatter(t_values, energies[:,0], t_values, energies[:,1])
+# ax.set_xlabel('Time [s]')
+# ax.set_ylabel('Energy [a.u.]')
+# ax.set_title('Landau-Zener Transition')
+# ax.legend(loc='upper right', bbox_to_anchor=(1, 0.75))
+# # plt.tight_layout()
+# fig.subplots_adjust(left=0.15, right=0.87, top=0.9, bottom=0.15)
+# plt.savefig('../doc/figs/avoided_crossing.pdf')
+# plt.show()
+# exit()
+
+t1, psi1, e1, nonint_e1 = landau_zener(10.0, 1.0)
+t2, psi2, e2, nonint_e2 = landau_zener(1.0, 1.0)
+p11 = np.abs(psi1[:, 0])**2
+p12 = np.abs(psi1[:, 1])**2
+p21 = np.abs(psi2[:, 0])**2
+p22 = np.abs(psi2[:, 1])**2
 # Plot results
-plt.plot(t_values, P1, label=r'$P_1(t)$')
-plt.plot(t_values, P2, label=r'$P_2(t)$')
-plt.xlabel('Time')
-plt.ylabel('Population')
-plt.legend()
-plt.title('Landau-Zener Transition')
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=find_figsize(1.2, 0.4), sharex=True)
+ax[0].plot(t1, p11, label=r'$\psi_0(t)$', color=b)
+ax[0].plot(t1, p12, label=r'$\psi_1(t)$', color=r)
+ax[1].plot(t2, p21, label=r'$\psi_0(t)$', color=b)
+ax[1].plot(t2, p22, label=r'$\psi_1(t)$', color=r)
+ax[0].set_title(r'$k = 10.0,\quad V=1.0$')
+ax[1].set_title(r'$k = 1.0,\quad V=1.0$')
+ax[0].set_xlabel('Time [s]')
+ax[1].set_xlabel('Time [s]')
+ax[0].set_ylabel('Population')
+ax[1].set_ylabel('Population')
+ax[1].legend(loc='upper right', bbox_to_anchor=(1, 0.75))
+plt.tight_layout()
+fig.subplots_adjust(left=0.1, right=0.9, top=0.85, bottom=0.15)
+fig.suptitle('Landau-Zener Transition')
+plt.savefig('../doc/figs/landau_zener.pdf')
 plt.show()
---
+++ src/utils/visualization.py
@@ -0,0 +1,102 @@
+import warnings
+warnings.filterwarnings('ignore', category=UserWarning) # Ignores a warnings due to scipy.special version dislike of current python3.10 installation
+import matplotlib.style
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.special
+import matplotlib
+import seaborn as sns
+
+
+import quantum_systems as qs        # Quantum systems library, will be swapped out with the new library at a later stage
+
+def find_figsize(width_scale=1, height_scale=1):
+    """Calculate the figsize based on the LaTeX scaling of \textwidth.
+
+    Args:
+        width_scale (int, optional): Scaling of \textwidth in width. Defaults to 1.
+        height_scale (int, optional): Scaling of \textwidth in height. Defaults to 1.
+
+    Returns:
+        figsize 
+    """
+    textwidth_pt = 418.25368
+    textwidth_in_inches = textwidth_pt * 0.01384
+    textheight_pt = 674.33032
+    textheight_in_inches = textheight_pt * 0.01384
+
+    figure_width = textwidth_in_inches * width_scale
+    figure_height = textheight_in_inches * height_scale
+
+    figsize = (figure_width, figure_height)
+    
+    return figsize
+
+
+def show_potential_with_spf():
+    """Show the potential and the single-particle functions."""
+    num_grid_points = 4_001
+    grid = np.linspace(-1.5, 10.0, num_grid_points)
+    D = 10
+    a = 0.5
+    # sns.set_theme()
+    matplotlib.style.use('seaborn-v0_8')
+    def compute_eigenenergies(c, D, l):
+        hnu = 2 * c * np.sqrt(D / 2)
+        E_n = np.zeros(l)
+        for n in range(l):
+            E_n[n] = hnu * (n + 0.5) - (c * hnu * (n + 0.5)**2) / np.sqrt(8 * D)
+
+        return E_n
+    def morse_spf(x, n, lmbda, x_e, c):
+        z = 2 * lmbda * np.exp(-c * (x - x_e))
+        return (
+            (scipy.special.factorial(n) * (2 * lmbda - 2 * n - 1) * c / scipy.special.gamma(2 * lmbda - n))**0.5 * 
+             z**(lmbda - n - 0.5) * np.exp(-z / 2) * scipy.special.genlaguerre(n, 2 * lmbda - 2 * n - 1)(z)
+        )
+    def morse_pot(x, D, a, x_e):
+        return D * (1 - np.exp(-a * (x - x_e)))**2
+    V = morse_pot(grid, D, a, 0)
+    E_n = compute_eigenenergies(a, D, 5)
+    # find crossing points
+    e0 = np.where(np.abs(V - E_n[0]) < 1e-2)
+    e0 = [324, 800]
+    e1 = [215, 1084]
+    e2 = [155, 1344]
+    e3 = [115, 1618]
+    e4 = [87, 1930]
+    lmbda = np.sqrt(2 * D) / a
+    spf = np.zeros((5, num_grid_points))
+    for i in range(5):
+        spf[i] = morse_spf(grid, i, lmbda, 0, a)
+    figsize=(10, 5)
+    # figsize = find_figsize(0.9, 0.3)
+    fig, ax = plt.subplots(ncols=2, nrows=1, figsize=figsize, sharex=True)
+    ax[0].plot(grid, V, label="Potential")
+    ax[0].set_title("Potential")
+    ax[0].set_xlabel("Position [a.u]")
+    ax[0].set_ylabel("Energy [a.u]")
+    ax[0].hlines(E_n[0], grid[e0[0]], grid[e0[1]], linestyles="--")
+    ax[0].hlines(E_n[1], grid[e1[0]], grid[e1[1]], linestyles="--")
+    ax[0].hlines(E_n[2], grid[e2[0]], grid[e2[1]], linestyles="--")
+    ax[0].hlines(E_n[3], grid[e3[0]], grid[e3[1]], linestyles="--")
+    ax[0].hlines(E_n[4], grid[e4[0]], grid[e4[1]], linestyles="--")
+    ax[0].axhline(D, linestyle="--", linewidth=1.5, color="black")
+    ax[0].legend(["Potential", "Energy levels"])
+    # ax[0].annotate(
+    #     r"D",
+    #     xy=(grid[-1], D),
+    #     xytext=(grid[-1000], D - 2),
+    #     arrowprops=dict(facecolor="black", arrowstyle="->"),
+    # )
+    ax[1].plot(grid, np.abs(spf[0])**2, grid, np.abs(spf[1])**2, grid, np.abs(spf[2])**2, grid, np.abs(spf[3])**2, grid, np.abs(spf[4])**2)
+    ax[1].set_title("Single-particle functions")
+    ax[1].set_xlabel("Position [a.u]")
+    ax[1].set_ylabel("Amplitude")
+    ax[1].legend(["n=0", "n=1", "n=2", "n=3", "n=4"])
+    fig.tight_layout()
+    plt.savefig('../../doc/figs/potential_spf.pdf')
+    plt.show()
+
+if __name__ == "__main__":
+    show_potential_with_spf()
